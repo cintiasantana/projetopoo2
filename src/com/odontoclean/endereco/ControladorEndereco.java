7455c7963d6f743c7b151528dd2d5a41e60511fb
Danilo Júnior <danilojuniorcardoso@gmail.com>
2015-11-07 13:29:58 -0200

    Implementação da classe ControladorEndereco e criação de um novo pacote.

--- src/com/odontoclean/endereco/ControladorEndereco.java
+++ src/com/odontoclean/endereco/ControladorEndereco.java
@@ -1,5 +1,39 @@
 package com.odontoclean.endereco;
 
-public class ControladorEndereco {
+import java.util.ArrayList;
 
+import com.odontoclean.paciente.util.CampoObritarorioInvalidoException;
+
+
+public class ControladorEndereco {
+private IRepositorioEndereco repositorioEndereco;
+	
+	public ControladorEndereco() {
+		this.repositorioEndereco = new RepositorioEnderecoArray();
+	}
+	public void cadastrar(Endereco endereco) throws EnderecoJaCadastradoException, 
+													CampoObritarorioInvalidoException  {
+		if (endereco == null) throw new IllegalArgumentException("Endereco Inválido.");
+	    if (endereco.getRua().equals("")) throw new CampoObritarorioInvalidoException("Rua");
+	    
+	    this.repositorioEndereco.cadastrar(endereco);
+	}
+	public void atualizar(Endereco endereco) throws EnderecoNaoEncontradoException, 
+													CampoObritarorioInvalidoException {
+		if (endereco == null) throw new IllegalArgumentException("Endereco Inválido.");
+	    if (endereco.getRua().equals("")) throw new CampoObritarorioInvalidoException("Rua");
+		this.repositorioEndereco.atualizar(endereco);
+	}
+	public void remover(Integer id) throws EnderecoNaoEncontradoException {
+		this.repositorioEndereco.remover(id);
+	}
+	public Endereco procurar(Integer id) throws EnderecoNaoEncontradoException  {
+		return this.repositorioEndereco.procurar(id);
+	}
+	public Endereco procurarPorCliente(Integer clienteId) throws EnderecoNaoEncontradoException  {
+		return this.repositorioEndereco.procurarPorCliente(clienteId);
+	}
+	public ArrayList<Endereco> listar() {
+		return this.repositorioEndereco.listar();
+	}
 }
