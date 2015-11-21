d2ee06067c23a4abb073fb71cb1be5f746e5e0df
Rafael <Rafael@DESKTOP-7MD485N>
2015-11-21 19:35:04 -0300

    Implementando o metodo cadastrar na classe RepositorioPacienteBDR

--- src/com/odontoclean/paciente/RepositorioPacienteBDR.java
+++ src/com/odontoclean/paciente/RepositorioPacienteBDR.java
@@ -1,13 +1,31 @@
 package com.odontoclean.paciente;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import javax.swing.JOptionPane;
+import com.odontoclean.jdbc.*;
 
 public class RepositorioPacienteBDR implements IRepositorioPaciente{
 
 	@Override
 	public void cadastrar(Paciente paciente)
 			throws PacienteJaCadastradoException {
-		// TODO Auto-generated method stub
+		try {
+
+			PreparedStatement pstmt;
+			pstmt = ConectaBD.AbriConexao().prepareStatement("INSERT INTO PACIENTE (CODIGO, NOME, CPF, TELEFONE, EMAIL, DATANASCIMENTO) values (?,?,?,?,?,?)");
+			pstmt.setString(1, ""); // NÃO ESTOU CONSEGUINDO PASSAR O PARAMETRO AQUI
+
+			pstmt.executeUpdate();
+
+			JOptionPane.showMessageDialog(null,"XML Gerado com sucesso!!!!");
+			pstmt.close();
+
+		} catch (SQLException e) {
+			JOptionPane.showMessageDialog(null,"Não foi possivel inserir o XML\n" + e);
+		}
 		
 	}
 
